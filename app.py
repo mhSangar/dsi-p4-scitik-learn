6a673f0a095782d30d0c0e4567b1e113de911401
Juan Machuca <juan0machuca@hotmail.com>
2019-04-15 18:06:00 +0200

    Load dataset & fit & predict & score

--- app.py
+++ app.py
@@ -2,9 +2,10 @@ import sys
 import os
 import logging
 import coloredlogs
-
-
-# Press F to pay respects
+import numpy as np
+from sklearn.datasets import load_iris
+from sklearn.model_selection import train_test_split
+from sklearn import svm #Support Vector Classification - estimador (implementa fit() y predict())
 
 
 logger = logging.getLogger(__name__)
@@ -17,8 +18,25 @@ def initLogger(level='INFO'):
 
 
 def main():
-    pass
-    
+    iris = load_iris()
+
+    # print (iris.data)
+    # print (iris.feature_names)
+    # print (iris.target)
+    # print (iris.target_names)
+
+    X_train, X_test, y_train, y_test = train_test_split(iris.data, iris.target)
+
+    #clasificador (utilizar frid search & cross validation para encontrar gamma)
+    clf = svm.SVC(gamma=0.001, C=100.)
+
+    model = clf.fit (X_train, y_train)
+
+    logger.debug ("Predicción: {output}".format(output = clf.predict(X_test)))
+
+    logger.debug ("Resultado real: {output}".format(output = y_test))
+
+    logger.debug ("Score: {output}".format(output = model.score(X_test, y_test)))
 
 if __name__ == '__main__':
     initLogger('DEBUG')
