4d19ebac3a901e2feb46734bf30cb6d9572b8d92
Juan Machuca <juan0machuca@hotmail.com>
2019-04-16 16:31:11 +0200

    Matrix confusion & accuracy, recall and precision scores & different algorithm evaluation

--- app.py
+++ app.py
@@ -5,7 +5,14 @@ import coloredlogs
 import numpy as np
 from sklearn.datasets import load_iris
 from sklearn.model_selection import train_test_split
-from sklearn import svm #Support Vector Classification - estimador (implementa fit() y predict())
+from sklearn import tree
+from sklearn import svm
+from sklearn.naive_bayes import GaussianNB
+from sklearn.neural_network import MLPClassifier
+from sklearn.metrics import confusion_matrix
+from sklearn.metrics import accuracy_score
+from sklearn.metrics import recall_score
+from sklearn.metrics import precision_score
 
 
 logger = logging.getLogger(__name__)
@@ -16,8 +23,30 @@ def initLogger(level='INFO'):
                         datefmt='%d/%m/%Y %H:%M:%S', level=level)
     logger.debug('Logger is active')
 
+def classifierResults(clf, dataset):
+
+    X_train, X_test, y_train, y_test = train_test_split(dataset.data, dataset.target)
+
+    model = clf.fit(X_train, y_train)
+
+    predict = clf.predict(X_test)
+
+    logger.debug("Predicción:\n{output}".format(output = predict))
+
+    logger.debug("Resultado real:\n{output}".format(output = y_test))
+
+    logger.debug("Score: {output}".format(output = model.score(X_test, y_test)))
+
+    logger.debug("Matriz de confusión:\n{output}".format(output = confusion_matrix(y_test, predict, labels=[0, 1 ,2])))
+
+    logger.debug("Accuracy: {output}".format(output = accuracy_score(y_test, predict)))
+
+    logger.debug("Recall: {output}".format(output = recall_score(y_test, predict, average='micro')))
+
+    logger.debug("Precision: {output}".format(output = precision_score(y_test, predict, average='micro')))
 
 def main():
+
     iris = load_iris()
 
     # print (iris.data)
@@ -25,18 +54,29 @@ def main():
     # print (iris.target)
     # print (iris.target_names)
 
-    X_train, X_test, y_train, y_test = train_test_split(iris.data, iris.target)
+    #Support Vector Classification
+    #Utilizar frid search & cross validation para encontrar gamma adecuado
 
-    #clasificador (utilizar frid search & cross validation para encontrar gamma)
+    logger.debug("------Algorithm: Decision Tree------")
+
+    clf = tree.DecisionTreeClassifier()
+    classifierResults(clf, iris)
+
+    logger.debug("------Algorithm: Support Vector Classification------")
+
     clf = svm.SVC(gamma=0.001, C=100.)
+    classifierResults(clf, iris)
 
-    model = clf.fit (X_train, y_train)
+    logger.debug("------Algorithm: Gaussian Naive Bayes------")
 
-    logger.debug ("Predicción: {output}".format(output = clf.predict(X_test)))
+    clf = GaussianNB()
+    classifierResults(clf, iris)
 
-    logger.debug ("Resultado real: {output}".format(output = y_test))
+    logger.debug("------Algorithm: Neuronal Netowrk------")
 
-    logger.debug ("Score: {output}".format(output = model.score(X_test, y_test)))
+    clf = MLPClassifier(max_iter=500)
+    classifierResults(clf, iris)
+
 
 if __name__ == '__main__':
     initLogger('DEBUG')
